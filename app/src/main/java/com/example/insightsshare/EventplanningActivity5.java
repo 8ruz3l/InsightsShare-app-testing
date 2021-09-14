401101727544d46e995db786d8b827dd2362e390
emilieiios <89915196+emilieiios@users.noreply.github.com>
2021-09-14 22:34:14 +0200

    frontend of settings and eventplanning build

--- app/src/main/java/com/example/insightsshare/EventplanningActivity5.java
+++ app/src/main/java/com/example/insightsshare/EventplanningActivity5.java
@@ -17,35 +17,9 @@ import com.example.insightsshare.databinding.ActivityEventplanning5Binding;
 
 public class EventplanningActivity5 extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityEventplanning5Binding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityEventplanning5Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_eventplanning5);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_eventplanning5);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        setContentView(R.layout.activity_eventplanning5);
     }
 }
