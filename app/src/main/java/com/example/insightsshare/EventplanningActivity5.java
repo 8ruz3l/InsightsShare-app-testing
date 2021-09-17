c59f626ec62b2e95770263ebd7e1f1f17f11f754
emilieiios <89915196+emilieiios@users.noreply.github.com>
2021-09-17 11:12:17 +0200

    "rebuild ListView - not final - RecyclerView intended"

--- app/src/main/java/com/example/insightsshare/EventplanningActivity5.java
+++ app/src/main/java/com/example/insightsshare/EventplanningActivity5.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
