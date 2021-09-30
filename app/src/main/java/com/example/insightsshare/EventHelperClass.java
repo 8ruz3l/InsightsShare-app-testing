bb6516838109d90ed3cff11c27832527d9576294
emilieiios <89915196+emilieiios@users.noreply.github.com>
2021-09-30 15:13:56 +0200

    deleted ListView in JavaClass

--- app/src/main/java/com/example/insightsshare/EventHelperClass.java
+++ app/src/main/java/com/example/insightsshare/EventHelperClass.java
@@ -1,2 +1,57 @@
-package com.example.insightsshare;public class EventHelperClass {
+package com.example.insightsshare;
+
+public class EventHelperClass {
+
+    String EventName, Date, Time, Location, MaxParticipants, Publish;
+
+    public EventHelperClass() {
+    }
+
+    public String getEventName() {
+        return EventName;
+    }
+
+    public void setEventName(String eventName) {
+        EventName = eventName;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getMaxParticipants() {
+        return MaxParticipants;
+    }
+
+    public void setMaxParticipants(String maxParticipants) {
+        MaxParticipants = maxParticipants;
+    }
+
+    public String getPublish() {
+        return Publish;
+    }
+
+    public void setPublish(String publish) {
+        Publish = publish;
+    }
 }
