38a19b9697b76f47b373719b36ecc79d6bc33f66
Nico Alexander <nico.alxr@gmail.com>
2021-10-04 15:04:33 +0200

    Added toolbar with back button for EventplanningActivity5 and SettingsActivity6

--- app/src/main/java/com/example/insightsshare/SettingsActivity6.java
+++ app/src/main/java/com/example/insightsshare/SettingsActivity6.java
@@ -3,12 +3,29 @@ package com.example.insightsshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SettingsActivity6 extends AppCompatActivity {
+    //Toolbar elements
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings6);
+
+        // Set up the toolbar
+        setSupportActionBar(findViewById(R.id.toolbar));
+        // Set activity title in toolbar
+        getSupportActionBar().setTitle(R.string.title_setting_toolbar);
+        //Toolbar back button
+        backButton = (ImageView) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
