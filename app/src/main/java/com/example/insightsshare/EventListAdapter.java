76cad2954c5b2b1c59e3539f8d4f364c99707ec1
Nico Alexander <nico.alxr@gmail.com>
2021-10-07 14:31:17 +0200

    New EventDetailsActivity, set on click listener to the listed events in recycler view, change event attribute name from eventParticipant to maxParticipants

--- app/src/main/java/com/example/insightsshare/EventListAdapter.java
+++ app/src/main/java/com/example/insightsshare/EventListAdapter.java
@@ -1,12 +1,14 @@
 package com.example.insightsshare;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -37,7 +39,16 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.Even
         holder.eventCreationDate.setText(eventItem.getEventCreationDate());
         holder.eventPlace.setText(eventItem.getEventPlace());
         holder.eventDate.setText(eventItem.getEventDate());
-        holder.eventParticipant.setText(eventItem.getEventParticipant());
+        holder.maxParticipants.setText(eventItem.getMaxParticipants());
+
+        holder.eventCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), EventDetailsActivity.class);
+                i.putExtra("eventId", eventItem.getEventId());
+                view.getContext().startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -47,18 +58,22 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.Even
 
     public static class EventViewHolder extends RecyclerView.ViewHolder {
 
+        CardView eventCard;
+
         TextView eventName, eventCreator, eventCreationDate, eventPlace, eventDate,
-                eventParticipant;
+                maxParticipants;
 
         public EventViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            eventCard = (CardView) itemView.findViewById(R.id.event_card);
 
             eventName = itemView.findViewById(R.id.event_name);
             eventCreator = itemView.findViewById(R.id.event_creator);
             eventCreationDate = itemView.findViewById(R.id.event_creation_date);
             eventPlace =  itemView.findViewById(R.id.event_place);
             eventDate = itemView.findViewById(R.id.event_date);
-            eventParticipant = itemView.findViewById(R.id.event_max_participant);
+            maxParticipants = itemView.findViewById(R.id.event_max_participants);
         }
     }
 }
