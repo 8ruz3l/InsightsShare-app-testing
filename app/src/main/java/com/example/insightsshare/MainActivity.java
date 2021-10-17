799c251ccd47b863348569b9748e26874636777d
paulchen63 <paulchen63mesteria@gmail.com>
2021-10-17 13:27:28 +0200

    Login finalize Commit

--- app/src/main/java/com/example/insightsshare/MainActivity.java
+++ app/src/main/java/com/example/insightsshare/MainActivity.java
@@ -1,38 +1,81 @@
 package com.example.insightsshare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextUtils;
 import android.view.View;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText loginEmail, loginPassword;
+    Button loginButton;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.buttonSignIn1);
-        button.setOnClickListener(new View.OnClickListener() {
+        loginEmail = findViewById(R.id.inputEmail2);
+        loginPassword = findViewById(R.id.inputPassword2);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), NavigationActivity.class ));
+            finish();
+        }
+
+        loginButton = (Button) findViewById(R.id.anmeldeButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, NavigationActivity.class);
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    loginEmail.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    loginPassword.setError("Password is required");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), NavigationActivity.class));
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        Button toRegButton = (Button) findViewById(R.id.backToAnmButton);
+        toRegButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, RegistrActivity.class);
                 startActivity(i);
             }
         });
-    }
-
-    public void changeRegistScreen(View v){
-        Intent changeRegist = new Intent(this, RegistrActivity.class);
-        startActivity(changeRegist);
-    }
-
-    public void anmelden(View v){
-
     }
 }
