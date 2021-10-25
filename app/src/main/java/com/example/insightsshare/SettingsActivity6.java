20009750c803c06ecc8602587eb9538d5d858e3e
Nico Alexander <nico.alxr@gmail.com>
2021-10-25 16:37:10 +0200

    Add image placeholder, adjust SettingActivity layout

--- app/src/main/java/com/example/insightsshare/SettingsActivity6.java
+++ app/src/main/java/com/example/insightsshare/SettingsActivity6.java
@@ -1,16 +1,35 @@
 package com.example.insightsshare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SettingsActivity6 extends AppCompatActivity {
-    //Toolbar elements
+    // Toolbar elements
     ImageView backButton;
+
+    // View elements
+    TextView username;
+
+    // Database elements
+    FirebaseDatabase database;
+    DatabaseReference userRef;
+
+    // Get current user
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +41,26 @@ public class SettingsActivity6 extends AppCompatActivity {
         //Toolbar back button
         backButton = (ImageView) findViewById(R.id.backButton);
         backButton.setOnClickListener( view -> onBackPressed());
+
+        // Get user id
+        database = FirebaseDatabase.getInstance("https://insightsshare-1e407-default-rtdb.europe-west1.firebasedatabase.app");
+        userRef = database.getReference().child("User").child(user.getUid());
+
+        username = findViewById(R.id.OutputMailAdress6);
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserClass userClass = snapshot.getValue(UserClass.class);
+
+                username.setText(userClass.getUsername());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public void logout(View view) {
